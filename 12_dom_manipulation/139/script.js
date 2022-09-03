cf13ab71043e3479de14f1f6a86e14e6ffd42df7
Alex Labanino <ghostrider6610@gmail.com>
2022-09-03 13:50:24 -0400

    event.key === "Enter" fixed

--- 12_dom_manipulation/139/script.js
+++ 12_dom_manipulation/139/script.js
@@ -1,28 +1,30 @@
-// var button = document.getElementsByTagName("button")[0];
-
-// button.addEventListener("mouseleave", function() {
-//   console.log("CLICK!!!!");
-// });
-
 var button = document.getElementById("enter");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-  // console.log(input.value)
-  if (input.value.length > 0) {
-    var li = document.createElement("li"); // you need to create a node first
-    li.appendChild(document.createTextNode(input.value)); // then append it to the ul
-    ul.appendChild(li);
-    input.value = ""; // clear input after submit
-  }
-})
+function inputLength() {
+  return input.value.length;
+}
 
-input.addEventListener("click", function() {
-  if (input.value.length > 0 && event.key === "Enter") {
-    var li = document.createElement("li"); // you need to create a node first
-    li.appendChild(document.createTextNode(input.value)); // then append it to the ul
-    ul.appendChild(li);
-    input.value = ""; // clear input after submit
+function createListElement() {
+  var li = document.createElement("li"); // you need to create a node first
+  li.appendChild(document.createTextNode(input.value)); // then append it to the ul
+  ul.appendChild(li);
+  input.value = ""; // clear input after submit
+}
+
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
   }
-})
+}
+
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.key === "Enter") {
+    createListElement();
+  }
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
