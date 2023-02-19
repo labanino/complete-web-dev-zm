e5aa67432afa7ee193553bc5670d3acf11c0f87e
Alex Labanino <ghostrider6610@gmail.com>
2023-02-19 12:36:45 -0500

    Building an app - part 2

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    </React.StrictMode>
+   <React.StrictMode>
+      <CardList robots={robots} />
+   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
