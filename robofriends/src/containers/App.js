e04c5d8b6fcf9a1d42603e4190dfa207c762aaa2
Alex Labanino <ghostrider6610@gmail.com>
2023-02-23 13:36:12 -0500

    Error Boundry in React

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -25,7 +26,7 @@ class App extends Component {
 
     render() {
         const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
+        const filteredRobots = robots.filter(robot => { 
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
         return !robots.length ?
@@ -35,7 +36,9 @@ class App extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
             );
